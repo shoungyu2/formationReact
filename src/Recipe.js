716862553407d6d616a78fe8dd0b67225858047d
shoungyu2 <ysabourin78@gmail.com>
2020-07-20 14:09:42 +0200

    Ajout edit et accès à la bdd

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,35 +2,36 @@ import React, { useState } from 'react';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Input
-  } from 'reactstrap';
+} from 'reactstrap';
 import './Recipe.css'
 
-export function Recipe(props){
+export function Recipe(props) {
 
-    const[editMode, setEditMode] = useState(false)
-    const[recipe,setRecipe] = useState(props.recipe);
-    const{id,name,picture,description}=props.recipe;
+    const [editMode, setEditMode] = useState(false);
+    const [recipe, setRecipe] = useState(props.recipe);
+    const { id, name, picture, description } = props.recipe;
 
-    return(
+    return (
         <div className="Recipe">
-            {  !editMode
-            ? 
-            <Card>
-                <Button onClick={()=>props.deleteRecipe && props.deleteRecipe(props.recipe.id)}>Delete</Button>
-                <Button onClick={()=>editMode && setEditMode(true)}> Edit </Button>
-                <CardBody>
-                <CardTitle>{props.recipe.name}</CardTitle>
-                <CardImg src={props.recipe.picture}></CardImg>
-                <CardText>{props.recipe.description}</CardText>
-                </CardBody>
-            </Card>
-            :
-            <Card>
-                <Input defaultValue={name} onChange={(event)=> setRecipe({...recipe, name: event.target.value})}></Input>
-                <Input defaultValue={picture} onChange={(event)=> setRecipe({...recipe, picture: event.target.value})}></Input>
-                <Input defaultValue={description} onChange={(event)=> setRecipe({...recipe, description: event.target.value})}></Input>
-                <Button onClick="">Edit</Button>
-            </Card>
+            {!editMode
+                ?
+                <Card>
+                    <Button onClick={() => setEditMode(!editMode)}> Editttt </Button>
+                    <Button onClick={() => props.deleteRecipe && props.deleteRecipe(props.recipe.id)}>Delete</Button>
+
+                    <CardBody>
+                        <CardTitle>{props.recipe.name}</CardTitle>
+                        <CardImg src={props.recipe.picture}></CardImg>
+                        <CardText>{props.recipe.description}</CardText>
+                    </CardBody>
+                </Card>
+                :
+                <Card>
+                    <Input defaultValue={name} onChange={(event) => setRecipe({ ...recipe, name: event.target.value })}></Input>
+                    <Input defaultValue={picture} onChange={(event) => setRecipe({ ...recipe, picture: event.target.value })}></Input>
+                    <Input defaultValue={description} onChange={(event) => setRecipe({ ...recipe, description: event.target.value })}></Input>
+                    <Button onClick={() =>{ props.updateRecipe(recipe);setEditMode(!editMode)}}>Edit</Button>
+                </Card>
             }
         </div>
     )
--- src/Recipes.js
+++ src/Recipes.js
@@ -1,25 +1,56 @@
-import React, { useState } from 'react';
-import {MOCK} from './Mock';
-import {Recipe} from './Recipe'
+import React, { useState, useEffect } from 'react';
+import { MOCK } from './Mock';
+import { Recipe } from './Recipe'
 import './Recipes.css'
+import './Recipe.css'
+import useAxios from 'axios-hooks';
 
-export function Recipes(props){
-    
-    const [recipeList, setRecipeList] = useState(MOCK);
+export const baseURL = "http://10.0.1.156:8080/api/v1"
 
-    function deleteRecipe(id){
+export function Recipes(props) {
+
+    const [{ data, loading, error }] = useAxios("http://10.0.1.156:8080/api/v1/recipes");
+
+    const [{ data: dataEdit }, executeEdit] = useAxios(
+        {
+            url: `${baseURL}/recipes`,
+            method: "PUT"
+        },
+        { manual: true }
+    );
+
+    const [{data : dataDelete}, executeDelete] = useAxios(
+        {
+            url: `${baseURL}/recipes`,
+            method: "DELETE"
+        },
+        { manual: true}
+    )
+
+    const [recipeList, setRecipeList] = useState(data || MOCK);
+    const [addMode, setAddMode] = useState(false);
+
+    useEffect(() => setRecipeList(data), [data, dataEdit])
+
+    function deleteRecipe(id) {
         setRecipeList(recipeList.filter(recipe => recipe.id !== id));
+        
     }
-    
-    function addRecipe(recipe){
+
+    function updateRecipe(recipe){
+        setRecipeList(recipeList.map(old => old.id===recipe.id ? recipe : old))
+        executeEdit({data:recipe});
+    }
+
+    function addRecipe(recipe) {
         setRecipeList(recipeList.add(recipe));
     }
 
-    return(
+    return (
         <div className="Recipes">
-        {recipeList.map(recipe => 
-            <Recipe recipe={recipe} deleteRecipe={deleteRecipe}></Recipe>
-        )}
+            {recipeList && recipeList.map(recipe =>
+                <Recipe recipe={recipe} deleteRecipe={deleteRecipe} updateRecipe={updateRecipe}></Recipe>
+            )}
         </div>
     );
 }
